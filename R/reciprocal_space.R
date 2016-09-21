170a86c11fa03a50c71c457ebd578c85181b8115
James Foadi <james_foadi@yahoo.co.uk>
2016-09-21 18:33:47 +0100

    3 functions added:
    1) load_anomalous_data
    2) plot_absorption_curves
    3) strufac

--- R/reciprocal_space.R
+++ R/reciprocal_space.R
@@ -65,3 +65,209 @@ scafac <- function(h,a,Zj,occj,Bj=NULL,k=ksigma)
   
   return(ff)
 }
+
+
+#' Load anomalous data for a specific chemical element
+#' 
+#' Returns a dataframe with f' and f'' at various wavelengths
+#' for the specific chemical element.
+#' 
+#' @param chem_el 1- or 2-letters character string. The chemical symbol of
+#'    interest.
+#' @return A dataframe with 3 columns, the specific wavelength in angstroms
+#'    (lambda), f' (f1) and f'' (f2).
+#' @examples 
+#' # Load anomalous data for Fe
+#' ano_Fe <- load_anomalous_data("Fe")
+#' print(ano_Fe[1:10,])
+#' @export
+load_anomalous_data <- function(chem_el)
+{
+  # Load specific dataset of f' and f"
+  idx <- which(names(anomalous_data) == chem_el)
+  ad <- anomalous_data[[idx]]
+  
+  return(ad)
+}
+
+
+#' Plot of absorption curves
+#' 
+#' Plot f' and f'' absorption curves for the specified chemical element.
+#' Curves can be plotted in specified wavelength regions using parameter
+#' "zoom".
+#' 
+#' @param chem_el 1- or 2-letters character string. The chemical symbol of
+#'    interest.
+#' @param zoom Real vector of length 2. The two values are the extremes of
+#'    the wavelength window inside which to plot the two curves. Default is
+#'    for both curves to be plotted across the full available range.
+#' @return Nothing, but causes a 2D plot to be displayed in a graphical window.
+#' @examples 
+#' # No zoom
+#' plot_absorption_curves("Fe")
+#' 
+#' # Zoom
+#' plot_absorption_curves("Fe",c(1,3))
+#' @export
+plot_absorption_curves <- function(chem_el,zoom=NULL)
+{
+  # Load table related to specific chemical element
+  ad <- load_anomalous_data(chem_el)
+  
+  # Limit plot to input range
+  if (!is.null(zoom))
+  {
+    lset <- which(ad$lambda >= zoom[1])
+    rset <- which(ad$lambda <= zoom[2])
+    jdx <- na.omit(match(lset,rset))
+  }
+  if (is.null(zoom)) jdx <- 1:length(ad[,1])
+  
+  # Range
+  tmp <- range(ad$f1[jdx],ad$f2[jdx])
+  
+  # Plot
+  plot(ad$lambda[jdx],ad$f1[jdx],type="l",
+       xlab=expression(paste(lambda,"(",ring(A),")")),
+       ylab=expression(paste(f,"(",e^{-phantom(0)},")")),
+       ylim=c(min(tmp),max(tmp)),main=chem_el)
+  points(ad$lambda[jdx],ad$f2[jdx],type="l")
+}
+
+
+#' Calculation of structure factors
+#' 
+#' Calculates structure factors corresponding to one or more 1D Miller indices,
+#' given the atomic content of one unit cell. Anomalous scattering can be
+#' included using logical flag "anoflag" (default is FALSE). Crystal structures
+#' are always considered with no symetry. Thus a 1D structure with the P-1
+#' symmetry will have to be expanded first with function "expand_to_cell".
+#' 
+#' @param h Real numeric. One or more 1D Miller indices.
+#' @param a A real number. The unit cell side length.
+#' @param vx0 Vector of real numerics. Atom positions in the asymmetric
+#'  unit.
+#' @param vZ Vector of integers. Atomic numbers of all atoms in the
+#'  asymmetric unit.
+#' @param vB Vector of real numerics. B factors for all atoms in the
+#'  asymmetric unit.
+#' @param vocc Vector of real numerics. Occupancies (value between 0
+#'  and 1) for all atoms in the asymmetric unit. In this function there
+#'  is no mechanism to check whether the occupancy is appropriate in case
+#'  the atom is at a special position.
+#' @param anoflag Logical variable. If TRUE it forces scattering factors
+#'    to include anomalous contributions. As a consequence, Friedel's
+#'    pairs will not be equal.
+#' @param aK Real numeric. This is a fudge factor included to decrease the
+#'    strength of the anomalous contributions. Without aK the strength is too
+#'    high for 1D structures, compared to real 3D structures. So aK helps
+#'    bringing down the anomalous contribution within the 5%-10% normally
+#'    met with large-size structures. The default value is aK=0.3 (anoK is
+#'    included as internal data).
+#' @param lbda Real numeric. This is the wavelength in angstroms. Its value
+#'    is important in relation to anomalous scattering.
+#' @param k A real number. It controls the standard deviation of the 
+#'    gaussian function describing the atom and, thus, the shape of the
+#'    associated peak. The standard deviation sigma is given by:
+#'          \code{sigma = k * sqrt(Z)}
+#'    The default value is k=0.05 (ksigma is included as internal data).
+#' @param f1f2out Logical variable. This variable controls output of a small
+#'    table of f' and f'' values for all chemical elements in the structure.
+#'    Default is for the table to be printed.
+#' @return A vector of complex numbers, the structure factors corresponding
+#'    to the Miller indices in input.
+#' @examples 
+#' # First create the crystal structure (P1)
+#' a <- 10
+#' vx0 <- c(1,4,6.5)
+#' vZ <- c(8,26,6)
+#' vB <- c(18,20,17)
+#' vocc <- c(1,1,1)
+#' lbda <- 1.7   # Close to Fe's absorption
+#' 
+#' # Miller indices (including DC (h=0) component)
+#' hidx <- 0:10
+#' 
+#' # Now structure factors without anomalous contribution
+#' F <- strufac(hidx,a,vx0,vZ,vB,vocc,lbda=lbda)
+#' print(length(F))  # Includes DC component (h=0)
+#' print(F[1])       # DC component is real
+#' print(Mod(F))     # Amplitudes decrease with increasing
+#'                   # resolution (Miller indices)
+#'                   
+#' # Now try with anomalous contributions
+#' F <- strufac(hidx,a,vx0,vZ,vB,vocc,lbda=lbda,anoflag=TRUE)
+#' print(F[0])  # DC component is not any longer real
+#' 
+#' @export
+strufac <- function(h,a,vx0,vZ,vB,vocc,anoflag=FALSE,
+                    aK=anoK,lbda=1.0,k=ksigma,f1f2out=TRUE)
+{
+  if (length(vx0) != length(vZ)) 
+    stop("Atomic center and atomic number lengths differ!")
+  if (length(vx0) != length(vB))
+    stop("Atomic center and B-factors lengths differ!")
+  if (length(vx0) != length(vocc))
+    stop("Atomic center and atomic occupanciess lengths differ!")
+  
+  # If anomalous flag is on collect f' and f" for each 
+  # atomic species
+  if (anoflag)
+  {
+    atomic_species <- unique(vZ)
+    idx <- match(atomic_species,atoms$Z)
+    AnoFrame <- data.frame()
+    for (i in idx)
+    {
+      chem_el <- names(anomalous_data)[i]
+      ad <- load_anomalous_data(chem_el)
+      tmp <- abs(ad$lambda-lbda)
+      jdx <- which(tmp == min(tmp))
+      AnoFrame <- rbind(AnoFrame,data.frame(Z=atoms$Z[i],f1=ad$f1[jdx],
+                                            f2=ad$f2[jdx]))
+    }
+    if (f1f2out) print(AnoFrame)
+  }
+  
+  # Quantities used if the anomalous flag is off
+  f1 <- 0
+  f2 <- 0
+  ph_ano <- 0
+  
+  # Calculate structure factors
+  FRe <- rep(0,times=length(h))
+  FIm <- rep(0,times=length(h))
+  for (j in 1:length(vZ))
+  {
+    # Scattering factor
+    f <- scafac(h,a,vZ[j],vocc[j],vB[j],k)
+    
+    # Anomalous contribution
+    if (anoflag)
+    {
+      idx <- match(vZ[j],AnoFrame$Z)
+      f1 <- aK*AnoFrame$f1[idx]
+      f2 <- aK*AnoFrame$f2[idx]
+      ph_ano <- 0.5*pi+2*pi*h*vx0[j]/a
+    }
+    
+    # Complex factors
+    ph_reg <- 2*pi*h*vx0[j]/a
+    regRe <- cos(ph_reg)
+    regIm <- sin(ph_reg)
+    anoRe <- cos(ph_ano)
+    anoIm <- sin(ph_ano)
+    
+    # Scattering coefficients
+    F1 <- f+f1
+    F2 <- f2
+    
+    # Sum of various components
+    FRe <- FRe+(F1*regRe+F2*anoRe)
+    FIm <- FIm+(F1*regIm+F2*anoIm)
+  }
+  F <- complex(real=FRe,imaginary=FIm,length.out=length(h))
+  
+  return(F)
+}
